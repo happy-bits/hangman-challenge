f479cbe06b7be46a2f0a4d70efdcd9dc2b20c4bf
Oscar Olsson <happybits@gmail.com>
2020-07-12 17:57:39 +0200

    -

--- Hangman.MyVersion.Core/Hangman.cs
+++ Hangman.MyVersion.Core/Hangman.cs
@@ -16,3 +16,34 @@ namespace Hangman.MyVersion.Core
         }
     }
 }
+
+
+
+using Ardalis.SmartEnum;
+
+namespace Hangman.Version01.Core
+{
+    public abstract class GameState : SmartEnum<GameState>
+    {
+        public static readonly GameState GameIsOn = new Response("GameIsOn", 1, "", Feeling.Neutral);
+        public static readonly GameState GameWon = new Response("GameWon", 2, "You won!", Feeling.Positive);
+        public static readonly GameState GameLost = new Response("GameLost", 3, "You lost!", Feeling.Negative);
+
+        public string Message { get; private set; }
+        public Feeling Feeling { get; private set; }
+
+        private GameState(string name, int value) : base(name, value)
+        {
+        }
+
+        private sealed class Response : GameState
+        {
+            public Response(string name, int nr, string message, Feeling feeling) : base(name, nr)
+            {
+                Message = message;
+                Feeling = feeling;
+            }
+        }
+    }
+
+}
